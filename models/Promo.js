5bb1416cbfe2115c0ca5f085899de63697d373d5
William Rowell <121638756+trafficBSolutions@users.noreply.github.com>
2026-09-15 12:16:51 -0400

    Add filename and type fields to promo schema

--- models/Promo.js
+++ models/Promo.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const promoSchema = new mongoose.Schema({
   title: String,
   url: String,
+  filename: String,
+  type: { type: String, enum: ['upload', 'youtube'], default: 'youtube' },
 });
 
 module.exports = mongoose.model('Promo', promoSchema);
